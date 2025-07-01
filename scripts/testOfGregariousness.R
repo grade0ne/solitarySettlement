43ad2b1ad65b0245510a524d74707e0927ea7e33
grade0ne <alex.mendelson.698@my.csun.edu>
2025-07-01 15:29:00 -0700

    final figures exported as 1200dpi tiffs

--- scripts/testOfGregariousness.R
+++ scripts/testOfGregariousness.R
@@ -16,8 +16,8 @@ data_percent <- data %>%
   mutate(
     Family = as.factor(Family),
     Treatment = as.factor(Treatment),
-    Treatment = fct_recode(Treatment, "Adult Tube" = "Adult"),
-    Treatment = fct_relevel(Treatment, "Adult Tube", "Mussel", "Control"),
+    Treatment = fct_recode(Treatment, "Conspecific" = "Adult"),
+    Treatment = fct_relevel(Treatment, "Conspecific", "Mussel", "Control"),
     Settlement = (Settled / Total) * 100
   )
 
@@ -102,6 +102,12 @@ ggplot(data_percent, aes(x = Treatment, y = Settlement)) +
     legend.title = element_blank()
   )
 
+
+
+ggsave("Figure 1.TIFF", plot = bar_plot,
+       width = 8.4, height = 8.4, units = "cm", dpi = 1200)
+
+
 #################################################
 # Box plot (t-test)
 
@@ -165,3 +171,35 @@ ggplot(data_means, aes(x = Treatment, y = mean)) +
     legend.title = element_blank()
   )
 
+bar_plot <- ggplot(data_means, aes(x = Treatment, y = mean)) +
+  geom_bar(stat = "identity", position = "dodge", fill = "gray92", color = "black", width = 0.5, linewidth = 0.3) +
+  geom_point(data = data_percent,
+             aes(x = Treatment, y = Settlement, shape = Family),
+             position = position_jitter(width = 0.075, height = 0),
+             size = 1, fill = "black", color = "black") +  
+  scale_shape_manual(values = c(1, 2, 16, 17)) +
+  geom_errorbar(aes(ymin = pmax(mean - ci, 0), ymax = pmin(mean + ci, 100)), width = 0.35, linewidth = 0.3) +
+  geom_text(data = y_pos_lmm, aes(x = Treatment, y = y_pos, label = Letter),
+            vjust = 0, size = 3) +  
+  labs(x = "", y = "Settlement at 5 dpf (%)") +
+  theme_minimal(base_size = 12) +  
+  theme(
+    panel.grid = element_blank(),
+    axis.line = element_line(size = 0.3),
+    axis.title.y = element_text(size = 9, margin = margin(r = 3)),
+    axis.text = element_text(size = 8, color = "black"),
+    legend.position = c(0.8, 0.85),
+    legend.background = element_rect(fill = "white", color = "white"),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 6),
+    legend.key.height = unit(0.4, "cm"),
+    legend.key.width = unit(0.3, "lines"),
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background = element_rect(fill = "white", color = NA),
+    axis.title.x = element_blank()
+  )
+
+
+
+ggsave("Figure 1.TIFF", plot = bar_plot,
+       width = 8.4, height = 8.4, units = "cm", dpi = 1200, compression = 'lzw')
